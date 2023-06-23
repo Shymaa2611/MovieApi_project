a3b549dd873ba135dd264a7455550f64195bf04d
Your Name <you@example.com>
2023-06-23 19:21:54 +0200

    create url for MovieDate_pk

--- pages/models.py
+++ pages/models.py
@@ -29,3 +29,5 @@ class Movie(models.Model):
     type=models.CharField(max_length=50,choices=Type)
     def __str__(self):
         return self.title
+    
+    
--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns=[
- path('',views.MoviesData.as_view(),name='movie')
+ path('',views.MoviesData.as_view(),name='movie'),
+ path('Movie<int:id>/',views.MovieData_pk.as_view(),name='movie_pk')
 ]
